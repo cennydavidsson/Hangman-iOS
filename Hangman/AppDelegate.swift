e402d59bfb9d3c5aa4cbef1e04cccf70691aac6d
Cenny Davidsson <cenny@me.com>
2017-02-18 23:58:34 +0100

    Initial commit

---
+++ Hangman/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Hangman
+//
+//  Created by Cenny Davidsson on 2017-02-18.
+//  Copyright © 2017 Cenny. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Hangman/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Cenny Davidsson on 2017-02-18.
+//  Copyright © 2017 Cenny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
